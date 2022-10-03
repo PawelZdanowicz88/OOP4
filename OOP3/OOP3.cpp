fda6068ed8bb452284abf806ff38bd40091e6495
PawelZdanowicz88 <zdanowiczpawel88@gmail.com>
2022-10-03 23:09:27 +0200

    Second commit

---
+++ OOP3/OOP3.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+
+class Figure
+{
+protected:
+    int sides_count = 0;
+    std::string name;
+
+public:
+    Figure()
+    {
+        sides_count = 0;
+    }
+    Figure(int sides_count)
+    {
+        sides_count = 0;
+    }
+    void get_sides_count()
+    {
+        std::cout << name << ": " << sides_count << std::endl;
+    }
+    void get_name()
+    {
+        name = "Фигура";
+    }
+};
+
+
+class Triangle : public Figure
+{
+public:
+    Triangle() : Figure()
+    {
+        sides_count = 3;
+    }
+    void get_name()
+    {
+        name = "Треугольник";
+    }
+};
+
+class Quadrangle: public Figure
+{
+public:
+    Quadrangle() : Figure()
+    {
+        sides_count = 4;
+    }
+    void get_name()
+    {
+        name = "Четырехугольник";
+    }
+};
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Количество сторон: " << std::endl;
+    Figure Basic;
+    Basic.get_name();
+    Basic.get_sides_count();
+    Triangle Tri;
+    Tri.get_name();
+    Tri.get_sides_count();
+    Quadrangle Qua;
+    Qua.get_name();
+    Qua.get_sides_count();
+    return 0;
+}
+
